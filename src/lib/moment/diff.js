4582e7dff8a02a7f5ebcd6fd54e96cdc25c3418d
Yan Nunes <yan@campai.com>
2019-02-01 12:27:22 +0100

    Add round option to moment

--- src/lib/moment/diff.js
+++ src/lib/moment/diff.js
@@ -1,11 +1,10 @@
 import absFloor from '../utils/abs-floor';
+import absRound from '../utils/abs-round';
 import { cloneWithOffset } from '../units/offset';
 import { normalizeUnits } from '../units/aliases';
 
-export function diff (input, units, asFloat) {
-    var that,
-        zoneDelta,
-        delta, output;
+export function diff(input, units, asFloat, shouldRound) {
+    var that, zoneDelta, delta, output, isMonth;
 
     if (!this.isValid()) {
         return NaN;
@@ -22,26 +21,45 @@ export function diff (input, units, asFloat) {
     units = normalizeUnits(units);
 
     switch (units) {
-        case 'year': output = monthDiff(this, that) / 12; break;
-        case 'month': output = monthDiff(this, that); break;
-        case 'quarter': output = monthDiff(this, that) / 3; break;
-        case 'second': output = (this - that) / 1e3; break; // 1000
-        case 'minute': output = (this - that) / 6e4; break; // 1000 * 60
-        case 'hour': output = (this - that) / 36e5; break; // 1000 * 60 * 60
-        case 'day': output = (this - that - zoneDelta) / 864e5; break; // 1000 * 60 * 60 * 24, negate dst
-        case 'week': output = (this - that - zoneDelta) / 6048e5; break; // 1000 * 60 * 60 * 24 * 7, negate dst
-        default: output = this - that;
+        case 'year':
+            output = monthDiff(this, that) / 12;
+            break;
+        case 'month':
+            output = monthDiff(this, that);
+            break;
+        case 'quarter':
+            output = monthDiff(this, that) / 3;
+            break;
+        case 'second':
+            output = (this - that) / 1e3;
+            break; // 1000
+        case 'minute':
+            output = (this - that) / 6e4;
+            break; // 1000 * 60
+        case 'hour':
+            output = (this - that) / 36e5;
+            break; // 1000 * 60 * 60
+        case 'day':
+            output = (this - that - zoneDelta) / 864e5;
+            break; // 1000 * 60 * 60 * 24, negate dst
+        case 'week':
+            output = (this - that - zoneDelta) / 6048e5;
+            break; // 1000 * 60 * 60 * 24 * 7, negate dst
+        default:
+            output = this - that;
     }
 
-    return asFloat ? output : absFloor(output);
+    return asFloat ? output : shouldRound ? Math.round(output) : absFloor(output);
 }
 
-function monthDiff (a, b) {
+function monthDiff(a, b) {
     // difference in months
-    var wholeMonthDiff = ((b.year() - a.year()) * 12) + (b.month() - a.month()),
-        // b is in (anchor - 1 month, anchor + 1 month)
-        anchor = a.clone().add(wholeMonthDiff, 'months'),
-        anchor2, adjust;
+    var wholeMonthDiff = (b.year() - a.year()) * 12 + (b.month() - a.month());
+
+    // b is in (anchor - 1 month, anchor + 1 month)
+    var anchor = a.clone().add(wholeMonthDiff, 'months');
+
+    var anchor2, adjust;
 
     if (b - anchor < 0) {
         anchor2 = a.clone().add(wholeMonthDiff - 1, 'months');
