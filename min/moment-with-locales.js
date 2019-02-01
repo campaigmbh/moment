b17aead1b72906c5d50291eb39812983f6af8b74
Yan Nunes <yan@campai.com>
2019-02-01 13:14:03 +0100

    fuck you moment

--- min/moment-with-locales.js
+++ min/moment-with-locales.js
@@ -1045,6 +1045,10 @@
         }
 
         dayOfMonth = Math.min(mom.date(), daysInMonth(mom.year(), value));
+
+        // Make Sure calculation of the end date of the month return to get date of the other month correctly
+        if ([29, 28, 30, 31].includes(dayOfMonth)) dayOfMonth = daysInMonth(mom.year(), value)
+
         mom._d['set' + (mom._isUTC ? 'UTC' : '') + 'Month'](value, dayOfMonth);
         return mom;
     }
